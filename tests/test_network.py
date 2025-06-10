d80c7ca55809a96e3e259826f586add4a0091655
Debasish Pal <48341250+debpal@users.noreply.github.com>
2025-06-10 18:24:45 +0300

    new methods

--- tests/test_network.py
+++ tests/test_network.py
@@ -9,30 +9,43 @@ def network():
     yield OptiDamTool.Network()
 
 
-def test_connectivity(
+def test_netwrok(
     network
 ):
 
     # data folder
     data_folder = os.path.join(os.path.dirname(__file__), 'data')
+    stream_file = os.path.join(data_folder, 'stream.shp')
+    stream_col = 'ws_id'
+    dam_list = [21, 22, 5, 31, 17, 24, 27, 2, 13, 1]
 
     # adjacent downstream connectivity
     output = network.connectivity_adjacent_downstream(
-        stream_file=os.path.join(data_folder, 'stream.shp'),
-        stream_col='ws_id',
-        dam_list=[21, 22, 5, 31, 17, 24, 27, 2, 13, 1]
+        stream_file=stream_file,
+        stream_col=stream_col,
+        dam_list=dam_list
     )
     assert output[17] == 21
     assert output[31] == -1
 
     # adjacent upstream connectivity
     output = network.connectivity_adjacent_upstream(
-        stream_file=os.path.join(data_folder, 'stream.shp'),
-        stream_col='ws_id',
-        dam_list=[21, 22, 5, 31, 17, 24, 27, 2, 13, 1]
+        stream_file=stream_file,
+        stream_col=stream_col,
+        dam_list=dam_list
     )
     assert output[17] == [1, 2, 5, 13]
     assert output[31] == []
+
+    # effective upstream drainage area
+    output = network.effective_drainage_area(
+        stream_file=stream_file,
+        stream_col=stream_col,
+        info_file=os.path.join(data_folder, 'stream_information.txt'),
+        dam_list=dam_list
+    )
+    assert output[17] == 2978593200
+    assert output[31] == 175558500
 
     # error for same stream identifiers in the input dam list
     with pytest.raises(Exception) as exc_info:
